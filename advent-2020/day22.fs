45da31e20fda6b3d5c1df91dea5c9f261a882f23
Steve Lowe <steve.lowe@mediatonic.co.uk>
2020-12-29 22:22:21 +0000

    day22-part2

--- advent-2020/day22.fs
+++ advent-2020/day22.fs
@@ -14,12 +14,8 @@ let getInput file =
 
     input.[0], input.[1]
 
-let playUntilWin deck1 deck2 =
-    let withCards card1 card2 deck =
-        let cards =
-            if card1 > card2 then [| card1; card2 |] else [| card2; card1 |]
-
-        cards |> Array.append deck
+let playCombat deck1 deck2 =
+    let withCards card1 card2 deck = [| card1; card2 |] |> Array.append deck
 
     let rec loop deck1 deck2 =
         match deck1, deck2 with
@@ -33,11 +29,51 @@ let playUntilWin deck1 deck2 =
                 if card1 > card2 then
                     deck1 |> (withCards card1 card2), deck2
                 else
-                    deck1, deck2 |> (withCards card1 card2)
+                    deck1, deck2 |> (withCards card2 card1)
 
             loop deck1 deck2
 
     loop deck1 deck2
+
+let playRecursiveCombat deck1 deck2 =
+    let withCards card1 card2 deck = [| card1; card2 |] |> Array.append deck
+
+    let rec loop deck1 deck2 seenDecks1 seenDecks2 =
+        match deck1, deck2 with
+        | _, [||] -> 1, deck1
+        | [||], _ -> 2, deck2
+        | _ ->
+            // if there was a previous round in this game that had exactly the same cards in the same order in the same players' decks,
+            //  the game instantly ends in a win for player 1.
+            match seenDecks1 |> List.contains deck1, seenDecks2 |> List.contains deck2 with
+            | true, _
+            | _, true -> 1, deck1
+            | _ ->
+                let seenDecks1 = deck1 :: seenDecks1
+                let seenDecks2 = deck1 :: seenDecks2
+                let card1, deck1 = deck1 |> Array.popHead
+                let card2, deck2 = deck2 |> Array.popHead
+
+                let player1WonRound =
+                    if deck1.Length >= card1 && deck2.Length >= card2 then
+                        // recurse to sub-game
+                        let winner, _ =
+                            loop (deck1 |> Array.take card1) (deck2 |> Array.take card2) [] []
+
+                        winner = 1
+                    else
+                        card1 > card2
+
+                let deck1, deck2 =
+                    if player1WonRound then
+                        deck1 |> (withCards card1 card2), deck2
+                    else
+                        deck1, deck2 |> (withCards card2 card1)
+
+                loop deck1 deck2 seenDecks1 seenDecks2
+
+    let _, deck = loop deck1 deck2 [] []
+    deck
 
 let getScore (deck: Deck) =
     let addScore (acc: int) (index, card) = acc + ((index + 1) * card)
@@ -47,7 +83,8 @@ let getScore (deck: Deck) =
     |> Array.indexed
     |> Array.fold addScore 0
 
-let part1 (deck1, deck2) = playUntilWin deck1 deck2 |> getScore
+let part1 (deck1, deck2) = playCombat deck1 deck2 |> getScore
 
 
-let part2 input = 2
+let part2 (deck1, deck2) =
+    playRecursiveCombat deck1 deck2 |> getScore
--- advent-2020/program.fs
+++ advent-2020/program.fs
@@ -56,5 +56,6 @@ let main _argv =
     solve 21 2 day21.getInput day21.part2 "gpgrb,tjlz,gtjmd,spbxz,pfdkkzp,xcfpc,txzv,znqbr"
 
     solve 22 1 day22.getInput day22.part1 35299
+    solve 22 2 day22.getInput day22.part2 33266
 
     0
