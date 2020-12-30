2c6d3c36050642bb128320a7ed1b14b572ad58f2
Steve Lowe <steve.lowe@mediatonic.co.uk>
2020-12-30 12:09:05 +0000

    day23-part1 - optimise

--- advent-2020/day23.fs
+++ advent-2020/day23.fs
@@ -10,32 +10,37 @@ let getInput file =
     |> Seq.toArray
 
 let playCups cups moves =
-    let rec findDestinationIndex cups current =
+    let rec findDestinationIndex cups picks current =
         let dest =
             match current with
             | 0 -> cups |> Array.max
             | _ -> current - 1
 
-        match cups |> Array.tryFindIndex (eq dest) with
-        | Some i -> dest, (i + 1)
-        | None -> findDestinationIndex cups dest
+        match picks |> Array.contains dest with
+        | true -> findDestinationIndex cups picks dest
+        | _ ->
+            match cups |> Array.tryFindIndex (eq dest) with
+            | Some i -> i
+            | None -> findDestinationIndex cups picks dest
 
     let rec loop cups movesLeft =
         match movesLeft with
         | 0 -> cups
         | _ ->
-            printfn "move %i cups %s" (moves - movesLeft + 1) (cups |> Array.take 8 |> Array.joinString " ")
-            let current, cups = cups |> Array.popHead
-            let picks, cups = ArrayLoop.subr cups 0 3
-            let dest, afterDestIndex = findDestinationIndex cups current
-            let beforeDest, afterDest = cups |> Array.splitAt afterDestIndex
+            // printfn "move %i cups %s" (moves - movesLeft + 1) (cups |> Array.take 8 |> Array.joinString " ")
+            let current = cups |> Array.head
+            let picks = ArrayLoop.sub cups 1 3
+            let destIndex = findDestinationIndex cups picks current
 
-            let nextCups =
-                Array.concat [| [| current |]
-                                beforeDest
-                                picks
-                                afterDest |]
-                |> ArrayLoop.fromIndex 1
+            let nextCups = Array.create cups.Length 0
+            nextCups.[cups.Length - 1] <- current
+            nextCups.[0] <- cups.[4]
+            // after picks to dest
+            Array.blit cups 5 nextCups 1 (destIndex - 4)
+            // picks
+            Array.blit picks 0 nextCups (destIndex - 4 + 1) 3
+            // after dest to end
+            Array.blit cups (destIndex + 1) nextCups destIndex (cups.Length - destIndex - 1)
 
             loop nextCups (movesLeft - 1)
 
